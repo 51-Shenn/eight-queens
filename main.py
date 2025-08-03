8ec09460d5fb10727ad32299d0b4f4274e6b5b95
Shenn <yshen0505@gmail.com>
2025-08-03 16:32:06 +0800

    Add function explanation

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import time
 import tracemalloc
 class EightQueens:
     def __init__(self, queens=None):
-        # if queens list is not defined, initialize as empty list
-        self.queens = queens if queens is not None else [] 
+        # if queens not given, initialize with -1 meaning empty
+        self.queens = [-1] * 8
+        if queens:
+            for i in range(min(len(queens), 8)):
+                self.queens[i] = queens[i]
         
     def set_queens(self, queens):
         self.queens = queens
@@ -23,6 +26,7 @@ class EightQueens:
         return True # if passed all constraints return true
     
     def display_board(self):
+        """Display the current position of queens in the chessboard"""
         print()
         self.board = [['-' for _ in range(8)] for _ in range(8)]
         for row, col in enumerate(self.queens):
@@ -33,6 +37,7 @@ class EightQueens:
         print()
 
     def win_or_lose(self, test_case_no):
+        """To determine whether the currect position of all queens is valid or invalid"""
         if self.is_valid_queen_placement():
             print(f"Test Case {test_case_no} Result: WIN  ✅")
             return 0
@@ -41,6 +46,15 @@ class EightQueens:
             return 1
     
     def place_queen(self, row, col):
+        """
+        Place or move a queen to a specified column in a given row
+        Use this function in algorithm implementation
+
+        Example: 
+            1. Queen currently at row 0, col 0
+            2. AFTER place_queen(row=0, col=2)
+            3. Queen moved to row 0, col 2
+        """
         if 0 <= row < 8 and 0 <= col < 8:
             self.queens[row] = col
             self.board[row][col] = 'Q'
